dc9d855108f2f6bbb9bf544c93c1ce0e901c8a77
Oloishorua <jacoboloishorua@gmail.com>
2024-03-11 05:49:18 +0300

    test init file storage

---
+++ tests/test_init.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Test for file storage'''
+
+import unittest
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.engine.file_storage import FileStorage
+
+class Test_Storage(unittest.TestCase):
+    def test_init(self):
+        bs = BaseModel()
+        self.assertEqual(bs.__class__, BaseModel)
+        self.assertisInstance(storage, FileStorage)
