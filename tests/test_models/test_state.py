a7850215eef304ae345cd71373a3305e1400a4cd
Oloishorua <jacoboloishorua@gmail.com>
2024-03-18 18:24:03 +0300

    Tests the state class

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,15 +2,47 @@
 '''Test for State class'''
 
 import unittest
+import os
+from datetime import datetime
 from models.base_model import BaseModel
 from models.state import State
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class Test_State(unittest.TestCase):
-    def test_string(self):
-        st = State()
-        self.assertEqual(st.name, "")
+    '''Tests for State class'''
 
-    def test_parnt(self):
+    def test_teardown(self):
+        '''tears test methods'''
+
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        '''reset storage'''
+
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_init(self):
+        '''test instantiation of state class'''
+
         st = State()
-        self.assertTrue(isinstance(st, BaseModel))
+        self.assertEqual(str(type(st)), "<class 'models.state.State'>")
+        self.assertIsInstance(st, State)
+        self.assertTrue(issubclass(type(st), BaseModel))
+
+    def test_attribs(self):
+        '''tests attributes of state class'''
+
+        attributes = storage.attributes()["State"]
+        st = State()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(st, k))
+            self.assertEqual(type(getattr(st, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
