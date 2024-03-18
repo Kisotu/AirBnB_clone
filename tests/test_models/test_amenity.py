5faa47c6cff15cd574fbd1b939dd278e1375f6c8
Oloishorua <jacoboloishorua@gmail.com>
2024-03-18 18:20:56 +0300

    tests for amenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,15 +2,44 @@
 '''Test for Amenity class'''
 
 import unittest
-from models.base_model import BaseModel
+from datetime import datetime
 from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+import os
 
 
 class Test_Amenity(unittest.TestCase):
-    def test_string(self):
-        amt = Amenity()
-        self.assertEqual(amt.name, "")
+    def test_test(self):
+        '''tears down test funcs'''
+        self.resetStorage()
+        pass
 
-    def test_parent(self):
-        amt = Amenity()
-        self.assertTrue(isinstance(amt, BaseModel))
+    def resetStorage(self):
+        '''Resets FileStorage'''
+
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_instantiation(self):
+        '''tests instantiation'''
+
+        am = Amenity()
+        self.assertEqual(str(type(am)), "<class 'models.amenity.Amenity'>")
+        self.assertIsInstance(am, Amenity)
+        self.assertTrue(issubclass(type(am), BaseModel))
+
+    def test_attribs(self):
+        '''tests attributes'''
+
+        attributes = storage.attributes()["Amenity"]
+        am = Amenity()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(am, k))
+            self.assertEqual(type(getattr(am, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
