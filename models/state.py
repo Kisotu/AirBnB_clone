b554e1c24913d483ee025f4bb598913b610879ee
Oloishorua <jacoboloishorua@gmail.com>
2024-03-18 18:19:16 +0300

    state model

--- models/state.py
+++ models/state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''Defines the state model'''
 
-from .base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
     '''The blueprint for a state obj'''
+
     name = ""
